105c18c7ca67a17d4127d13d08a97061406774d7
DebasishMohanta <debasish2018rta@gmail.com>
2021-07-30 08:05:33 +0530

    added HtmlPages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'html_pages/home'
+  get 'html_pages/help'
   root "application#hello"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
